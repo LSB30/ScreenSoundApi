0639ca0d8ac2d48c0de3f234aaaf5b47e54f20cd
Lucas Santana <122113277+llink7@users.noreply.github.com>
2023-08-07 13:38:37 -0300

    ✅ done

---
+++ ScreenSoundApi/Modelos/MusicasPreferidas.cs
@@ -0,0 +1,48 @@
+﻿
+
+using System.Text.Json;
+
+namespace ScreenSoundApi.Modelos;
+
+internal class MusicasPreferidas
+{
+    public string? Nome { get; set; }
+    public List<Music>? ListaDeMusicasFavoritas { get; }
+
+    public MusicasPreferidas(string nome)
+    {
+        Nome = nome;
+        ListaDeMusicasFavoritas = new();
+    }
+
+    public void AdicionarMusicasFavoritas(Music musica)
+    {
+        ListaDeMusicasFavoritas?.Add(musica);
+    }
+
+    public void ExibirMusicasFavoritas()
+    {
+        Console.WriteLine($"Essas são as Musicas Favoritas de {Nome}");
+
+        foreach(var musica in ListaDeMusicasFavoritas)
+        {
+            Console.WriteLine($"=> {musica.Nome} de {musica.Artista}");
+        }
+
+        Console.WriteLine();
+    }
+
+    public void GerarArquivoJson()
+    {
+        string json = JsonSerializer.Serialize(new
+        {
+            nome = Nome,
+            musicas = ListaDeMusicasFavoritas
+        });
+
+        string nomeDoArquivo = $"musicas-favoritas-{Nome}.json";
+
+        File.WriteAllText(nomeDoArquivo, json);
+        Console.WriteLine($"O arquivo JSON foi criado com Sucesso! {Path.GetFullPath(nomeDoArquivo)}");
+    }
+}
--- ScreenSoundApi/Program.cs
+++ ScreenSoundApi/Program.cs
@@ -12,6 +12,25 @@ using (HttpClient client = new HttpClient())
         //LinqOrder.ExibirListaDeArtistaOrdenados(musicas);
         //LinqFilter.FiltrarArtistasPeloGeneroMusical(musicas, "rock");
         LinqFilter.FiltrarMusicasDoArtista(musicas, "Lil Peep");
+
+        var musicasPreferiasDoLucas = new MusicasPreferidas("Lucas");
+        musicasPreferiasDoLucas.AdicionarMusicasFavoritas(musicas[30]);
+        musicasPreferiasDoLucas.AdicionarMusicasFavoritas(musicas[7]);
+        musicasPreferiasDoLucas.AdicionarMusicasFavoritas(musicas[10]);
+        musicasPreferiasDoLucas.AdicionarMusicasFavoritas(musicas[777]);
+
+        musicasPreferiasDoLucas.ExibirMusicasFavoritas();
+
+        //var musicasPreferiasDaJasna = new MusicasPreferidas("Jasna");
+        //musicasPreferiasDaJasna.AdicionarMusicasFavoritas(musicas[330]);
+        //musicasPreferiasDaJasna.AdicionarMusicasFavoritas(musicas[27]);
+        //musicasPreferiasDaJasna.AdicionarMusicasFavoritas(musicas[130]);
+        //musicasPreferiasDaJasna.AdicionarMusicasFavoritas(musicas[33]);
+
+        //musicasPreferiasDaJasna.ExibirMusicasFavoritas();
+
+        musicasPreferiasDoLucas.GerarArquivoJson();
+
     }
     catch (Exception ex) {
         Console.WriteLine($"Temos um problema: {ex.Message}");
